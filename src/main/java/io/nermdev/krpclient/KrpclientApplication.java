aa9dfdeaea684dd6384843b6c8f30ebd56d0c9b0
dedovicnermin <dedovicnermin@outlook.com>
2022-12-09 11:28:39 -0600

    005

--- src/main/java/io/nermdev/krpclient/KrpclientApplication.java
+++ src/main/java/io/nermdev/krpclient/KrpclientApplication.java
@@ -5,12 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KrpclientApplication {
-
-    static final String TOPIC = "rx.csx.store.000.test";
-    static final String BOOTSTRAP_SERVERS = "kafka.store.svc.cluster.local:9072";
-    static final String CLIENT_ID = "android-generator";
-    
-
     public static void main(String[] args) {
         SpringApplication.run(KrpclientApplication.class, args);
     }
